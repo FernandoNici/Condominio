f2199472d051078e2977774947e32efd388ea46c
FernandoNici <fernando.nici@gmail.com>
2015-10-20 20:15:03 -0200

    Inicio do Projeto

---
+++ src/condominio/model/Condomino.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condominio.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Nando
+ */
+public class Condomino {
+    private String nome;
+    private String sobreNome;
+    private String RG;
+    private String CPF;
+    private Long telefone;
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setLastName(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+}
